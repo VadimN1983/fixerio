52be4ec68b0cdd38a4308a688d94e29e5e1da885
Vadim A. Nikitin <nikitin.vadim@gmail.com>
2021-04-06 15:19:30 +0300

    Fix

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'fixerio.php';
 
+
+
+use \Thelema\Forex as FX;
+
+
+
 echo '<pre>';
-print_r(\Thelema\Forex\FixerIO::getInstance()->symbols());
+print_r(FX\FixerIO::getInstance()->symbols());
 echo '</pre>';
